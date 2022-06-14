c9e0880632a0a6dbbb55ac4413049f8cd764b88b
gfpkelly1986 <gfpkelly@gmail.com>
2022-06-14 19:12:43 +0000

    Complete left movemen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
 console.log('Console Working');
 
 const gameBoard = document.getElementById('game_board');
-const ship = document.getElementById('ship');
+let ship = document.getElementById('ship');
 
 /* Add event listener for any key pressed, getComputedstyle() returns a CSSStyleDeclaration Object of styles relating to element id='ship'*/
 window.addEventListener('keydown', function(event) {
-    let left = parseInt(window.getComputedStyle(ship).getPropertyValue('left'));
-    console.log(left);
+    let leftPosition = parseInt(window.getComputedStyle(ship).getPropertyValue('left'));
     /*Left direction movement*/
-    if(event.key === 'ArrowLeft'){
-        console.log('pressed left, position is: ' + left);
+    if(event.key === 'ArrowLeft' && leftPosition > 1){
+        ship.style.left = leftPosition - 5 + 'px';
+        console.log(ship.style.left);
     }
 });
