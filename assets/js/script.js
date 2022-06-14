e38c8feee7145ba710646d60c81e894e09473fd8
gfpkelly1986 <gfpkelly@gmail.com>
2022-06-14 18:29:46 +0000

    Add left condition and test with console.log

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,12 @@ console.log('Console Working');
 const gameBoard = document.getElementById('game_board');
 const ship = document.getElementById('ship');
 
+/* Add event listener for any key pressed, getComputedstyle() returns a CSSStyleDeclaration Object of styles relating to element id='ship'*/
 window.addEventListener('keydown', function(event) {
     let left = parseInt(window.getComputedStyle(ship).getPropertyValue('left'));
-    console.log('left');
+    console.log(left);
+    /*Left direction movement*/
+    if(event.key === 'ArrowLeft'){
+        console.log('pressed left, position is: ' + left);
+    }
 });
