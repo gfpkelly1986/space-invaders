d8d0fa3fceb21ceead828cb13da3400b26fb5f78
gfpkelly1986 <gfpkelly@gmail.com>
2022-06-14 19:35:41 +0000

    Complete left and right movement, no errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,16 @@ let ship = document.getElementById('ship');
 
 /* Add event listener for any key pressed, getComputedstyle() returns a CSSStyleDeclaration Object of styles relating to element id='ship'*/
 window.addEventListener('keydown', function(event) {
-    let leftPosition = parseInt(window.getComputedStyle(ship).getPropertyValue('left'));
+    let gameBoardWidth = document.getElementById('game_board').clientWidth;
+    let shipPosition = parseInt(window.getComputedStyle(ship).getPropertyValue('left'));
     /*Left direction movement*/
-    if(event.key === 'ArrowLeft' && leftPosition > 1){
-        ship.style.left = leftPosition - 5 + 'px';
+    if(event.key === 'ArrowLeft' && shipPosition > 1){
+        ship.style.left = shipPosition - 5 + 'px';
+        console.log(ship.style.left);
+    }
+    /* Right direction movement */
+    if(event.key === 'ArrowRight' && shipPosition < gameBoardWidth - 32){
+        ship.style.left = shipPosition + 5 + 'px';
         console.log(ship.style.left);
     }
 });
