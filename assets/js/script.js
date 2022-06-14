2cd6c1aad8d53188a0f47d45b8d79d9b85e18247
gfpkelly1986 <gfpkelly@gmail.com>
2022-06-14 18:10:03 +0000

    Add window event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
 console.log('Console Working');
+
+const gameBoard = document.getElementById('game_board');
+const ship = document.getElementById('ship');
+
+window.addEventListener('keydown', function(event) {
+    let left = parseInt(window.getComputedStyle(ship).getPropertyValue('left'));
+    console.log('left');
+});
