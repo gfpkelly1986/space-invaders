aa15fc648adcadbdfedaf89d955994ba207b80cf
gfpkelly1986 <gfpkelly@gmail.com>
2022-06-16 20:47:41 +0000

    Add single laser shot to game board.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,17 @@ console.log('Console Working');
 const gameBoard = document.getElementById('game_board');
 let ship = document.getElementById('ship');
 
+// let laser = false;
+// let activeLaser = document.getElementById('shoot'); 
+// activeLaser.addEventListener('click', clickHandler);
+
+
 /* Add event listener for any key pressed, getComputedstyle() returns a CSSStyleDeclaration Object of styles relating to element id='ship'*/
 window.addEventListener('keydown', function(event) {
     let gameBoardWidth = document.getElementById('game_board').clientWidth;
     let shipPosition = parseInt(window.getComputedStyle(ship).getPropertyValue('left'));
+    
+    
     /*Left direction movement*/
     if(event.key === 'ArrowLeft' && shipPosition > 1){
         ship.style.left = shipPosition - 5 + 'px';
@@ -17,4 +24,10 @@ window.addEventListener('keydown', function(event) {
         ship.style.left = shipPosition + 5 + 'px';
         console.log(ship.style.left);
     }
+    if(event.code == 'Space'){
+        let laser = document.createElement('div');
+        laser.classList.add('laser_beam');
+        gameBoard.appendChild(laser);
+    }
 });
+
