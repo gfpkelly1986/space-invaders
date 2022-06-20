da50096c56dbce1727ff022f4e76bea4adb65501
gfpkelly1986 <gfpkelly@gmail.com>
2022-06-20 09:28:42 +0000

    Add controller shoot functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,10 @@ console.log('Console Working');
 const gameBoard = document.getElementById('game_board');
 let ship = document.getElementById('ship');
 
-// let laser = false;
-// let activeLaser = document.getElementById('shoot'); 
-// activeLaser.addEventListener('click', clickHandler);
-
-
 //Add event listener for any key pressed, getComputedstyle() returns a CSSStyleDeclaration Object of styles relating to element id='ship'
 window.addEventListener('keydown', function(event) {
     let gameBoardWidth = document.getElementById('game_board').clientWidth;
     let shipPosition = parseInt(window.getComputedStyle(ship).getPropertyValue('left'));
-    
     
     //Left direction movement
     if(event.key === 'ArrowLeft' && shipPosition > 1){
@@ -31,7 +25,7 @@ window.addEventListener('keydown', function(event) {
         gameBoard.appendChild(laser);
     //create a setInterval function that runs every 10 milliseconds, controls the removal of aliens if hit by the laser also.     
         let shootLaser = setInterval(function(){
-            let enemies =document.getElementsByClassName('enemy_container');
+            let enemies = document.getElementsByClassName('enemy_container');
             for(i = 0; i < enemies.length; i++){
                 let alien = enemies[i];
                 let alienPosition = alien.getBoundingClientRect();
@@ -57,10 +51,44 @@ window.addEventListener('keydown', function(event) {
 
             laser.style.left = shipPosition + 10 + 'px';
             laser.style.bottom = laserBottom + 3 + 'px';
-        }, 10);  // end shootLaser setInterval function
+        });  // end shootLaser setInterval function
     } // end gameplay for spacebar pressed.
 
 }); //end event listener for keydown function
+
+const increaseAliens = setInterval(function(){
+    let ufo = document.createElement('div');
+    ufo.classList.add('enemy_container');
+    let boardWidth = parseInt(gameBoard.getBoundingClientRect().width);
+    ufo.style.left = Math.floor(Math.random() * (boardWidth - 30))  + 'px';
+    gameBoard.appendChild(ufo);
+}, 3000);
+
+  const fallingAliens = setInterval(function(){
+    let ufo = document.getElementsByClassName('enemy_container');
+        for(i = 0; i < ufo.length; i++){
+            let fallingUfo = ufo[i];
+            let ufoTop = parseInt(window.getComputedStyle(fallingUfo).getPropertyValue('top'));
+            let boardBottom = parseInt(gameBoard.getBoundingClientRect().bottom - 150);
+            let shipCoordinates = ship.getBoundingClientRect();
+            let fallingCoordinates = fallingUfo.getBoundingClientRect();
+            if(ufoTop >= boardBottom){
+                clearInterval(fallingAliens);
+                window.location.reload();
+                gameBoard.removeChild(fallingUfo);
+                console.log('game over');
+            }
+            if( fallingCoordinates.bottom >= shipCoordinates.top &&
+                fallingCoordinates.left -10 <= shipCoordinates.left &&
+                fallingCoordinates.right -10<= shipCoordinates.right){
+                clearInterval(fallingAliens);
+                window.location.reload();
+                gameBoard.removeChild(fallingUfo);
+                console.log('game over');
+            }
+            fallingUfo.style.top = ufoTop + 15 + 'px';
+        }
+},4000)
 
 //Mobile controls
 function moveLeftMobile(){
@@ -73,47 +101,42 @@ function moveRightMobile(){
     ship.style.left = shipPosition + 5 + 'px';
 }
 
-
-//function called on button click to run the game
-function startGame(){
-
-    const increaseAliens = setInterval(function(){
-        let ufo = document.createElement('div');
-        ufo.classList.add('enemy_container');
-        let boardWidth = parseInt(gameBoard.getBoundingClientRect().width);
-        ufo.style.left = Math.floor(Math.random() * (boardWidth - 30))  + 'px';
-        gameBoard.appendChild(ufo);
-    }, 1000);
-
-        fallingAliens = setInterval(function(){
-        let ufo = document.getElementsByClassName('enemy_container');
-
-        if(ufo != undefined){
-            for(i = 0; i < ufo.length; i++){
-                let fallingUfo = ufo[i];
-                let ufoTop = parseInt(window.getComputedStyle(fallingUfo).getPropertyValue('top'));
-                let boardBottom = parseInt(gameBoard.getBoundingClientRect().bottom - 150);
-                let shipCoordinates = ship.getBoundingClientRect();
-                let fallingCoordinates = fallingUfo.getBoundingClientRect();
-                if(ufoTop >= boardBottom){
-                    clearInterval(fallingAliens);
-                    window.location.reload();
-                    gameBoard.removeChild(fallingUfo);
-                    console.log('game over');
-                }
-                if( fallingCoordinates.bottom >= shipCoordinates.top &&
-                    fallingCoordinates.left -10 <= shipCoordinates.left &&
-                    fallingCoordinates.right -10<= shipCoordinates.right){
-                    clearInterval(fallingAliens);
-                    window.location.reload();
-                    gameBoard.removeChild(fallingUfo);
-                    console.log('game over');
-                }
-                fallingUfo.style.top = ufoTop + 15 + 'px';
-            }
+function mobileShoot(){
+    let shipPosition = parseInt(window.getComputedStyle(ship).getPropertyValue('left'));
+    let laser = document.createElement('div');
+    laser.classList.add('laser_beam');
+    gameBoard.appendChild(laser);
+    let shootLaser = setInterval(function(){
+        let enemies = document.getElementsByClassName('enemy_container');
+        for(i = 0; i < enemies.length; i++){
+            let alien = enemies[i];
+            let alienPosition = alien.getBoundingClientRect();
+            let laserPosition = laser.getBoundingClientRect();
+            if (
+                laserPosition.left >= alienPosition.left &&
+                laserPosition.right <= alienPosition.right &&
+                laserPosition.top - 6 <= alienPosition.top &&
+                laserPosition.bottom - 6 <= alienPosition.bottom)
+                {
+                    laser.parentElement.removeChild(alien);
+                } 
         }
-    },4000)
+        let laserBottom = parseInt(window.getComputedStyle(laser).getPropertyValue('bottom'));
+        let gameBoardHeight = gameBoard.getBoundingClientRect().height;
+
+        if(laserBottom + 40 >= gameBoardHeight){
+            clearInterval(shootLaser);
+            gameBoard.removeChild(laser);
+        }
+
+        laser.style.left = shipPosition + 10 + 'px';
+        laser.style.bottom = laserBottom + 3 + 'px';
+    });
 }
+
+
+    
+
 
 
 
